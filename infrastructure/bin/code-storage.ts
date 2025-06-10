369e7083ceb73a4616a7583b8f57967fce478c62
takumasa0222 <t.tonnbo0222@gmail.com>
2025-06-11 07:38:57 +0900

    modified code-storage.ts for fixing scope issue

--- infrastructure/bin/code-storage.ts
+++ infrastructure/bin/code-storage.ts
@@ -1,17 +1,21 @@
 #!/usr/bin/env node
 import * as cdk from 'aws-cdk-lib';
+import { Construct } from 'constructs';
 import { BackendCodeS3Stack } from '../lib/stacks/repository-s3-stack';
+import { StageStackProps } from '../lib/types/stack-props';
 
 const app = new cdk.App();
 const stage = app.node.tryGetContext('stage');
 const appname = app.node.tryGetContext('appname');
 
-new BackendCodeS3Stack(app, `${appname}-BackendCodeS3-${stage}`, 
-    {
-      stage: stage,
-      appname: appname,
-    }
-    
-);
+class CodeStorageStack extends cdk.Stack {
+  constructor(scope: Construct, id: string, props: StageStackProps) {
+    super(scope, id, props);
+    new BackendCodeS3Stack(this, `${appname}-BackendCodeS3`, props);
+  }
+}
 
-
+new CodeStorageStack(app, `${appname}-CodeStorageStack-${stage}`, {
+  stage,
+  appname,
+});
