b761e1488641032ef29d3e498b02dd96d1529296
takumasa0222 <t.tonnbo0222@gmail.com>
2025-06-19 16:11:53 +0900

    modified

--- infrastructure/lib/constants/table.constants.ts
+++ infrastructure/lib/constants/table.constants.ts
@@ -1,6 +1,6 @@
 export const TABLE_INIT = {
 	BASE_NAME: 'TableInitializer',
 	DEFAULT_TABLE_NAME: 'greetings',
-	CODE_PATH: '../../../lambda/init-table',
+	CODE_PATH: '../../../lambda/init-table/dist',
 	HANDLER: 'handler.handler'
 }
